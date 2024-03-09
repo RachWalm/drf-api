562be0c25d9c3aa3cf62990c2938ac236e77251e
Rachel Walmsley <rachwalm75@gmail.com>
2024-03-09 11:12:12 +0000

    Add different default image and migrate

--- profiles/models.py
+++ profiles/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=255, blank=True)
     content = models.TextField(blank=True)
     image = models.ImageField(
-        upload_to='images/', default='../default_profile_fupmpz')
+        upload_to='images/', default='../default_profile_dsxegu')
     
     class Meta:
         ordering = ['-created_at']
